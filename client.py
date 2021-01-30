dbd0c76710868333c23856f7953c1af959b7c47f
Pranav Pooruli <pranav.pooruli@gmail.com>
2021-01-29 20:39:16 -0600

    edit server.py, fix client.py

--- client.py
+++ client.py
@@ -11,33 +11,33 @@ cam = cv2.VideoCapture(0)
 
 
 def send_data():
-    while cam.isOpened():
-        ret, frame = cam.read()
-        if connected:
-            pickled = pickle.dumps(frame)
-            sock.sendall(struct.pack('L', len(pickled)) + pickled)
-            sock.recv(1024)
+	while cam.isOpened():
+		ret, frame = cam.read()
+		if connected:
+			pickled = pickle.dumps(frame)
+			sock.sendall(struct.pack('L', len(pickled)) + pickled)
+			sock.recv(1024)
 
 
 def connect():
-    global connected
-    while True:
-        if connected:
-            break
-        else:
-            try:
-                print('not connected')
-                print('trying to connect')
-                sock.connect(('192.168.1.128', 8888))
-                print('connected')
-                connected = True
-            except:
-                print('host is not online, trying again...')
+	global connected
+	while True:
+		if connected:
+			break
+		else:
+			try:
+				print('not connected')
+				print('trying to connect')
+				sock.connect(('192.168.1.128', 8888))
+				print('connected')
+				connected = True
+			except:
+				print('host is not online, trying again...')
 
 
 if __name__ == '__main__':
-    threads = [threading.Thread(target=connect),
-               threading.Thread(target=send_data)]
-    for thread in threads:
-        thread.start()
-    print('started threads')
+	threads = [threading.Thread(target=connect),
+			   threading.Thread(target=send_data)]
+	for thread in threads:
+		thread.start()
+	print('started threads')
--- server.py
+++ server.py
@@ -15,54 +15,54 @@ connected = False
 
 
 def recv_data():
-    while True:
-        if connected:
-            data = receive_arr()
-            show_data(data)
-        else:
-            print('no data to show')
+	while True:
+		if connected:
+			data = receive_arr()
+			show_data(data)
+		else:
+			print('no data to show')
 
 
 def show_data(data):
-    if cv2.waitKey(10) == ord('q'):
-        quit()
-    cv2.imshow('SpyThing', data)
+	if cv2.waitKey(10) == ord('q'):
+		quit()
+	cv2.imshow('SpyThing', data)
 
 
 def receive_arr():
-    conn = conns[0]
-    data = b''
-    payload_size = struct.calcsize('L')
-    while len(data) < payload_size:
-        data += conn.recv(4096)
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack('L', packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-    conn.send(b'RECVD')
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    screen_w, screen_h = size()
-    return cv2.resize(frame, (int(screen_w * 0.75), int(screen_h * 0.75)), interpolation=cv2.INTER_AREA)
+	conn = conns[0]
+	data = b''
+	payload_size = struct.calcsize('L')
+	while len(data) < payload_size:
+		data += conn.recv(4096)
+	packed_msg_size = data[:payload_size]
+	data = data[payload_size:]
+	msg_size = struct.unpack('L', packed_msg_size)[0]
+	while len(data) < msg_size:
+		data += conn.recv(4096)
+	conn.send(b'RECVD')
+	frame_data = data[:msg_size]
+	data = data[msg_size:]
+	frame = pickle.loads(frame_data)
+	screen_w, screen_h = size()
+	return cv2.resize(frame, (int(screen_w * 0.75), int(screen_h * 0.75)), interpolation=cv2.INTER_AREA)
 
 
 def accept_connection():
-    global connected
-    while True:
-        if connected:
-            break
-        else:
-            conn, addr = sock.accept()
-            print(addr, 'has connected')
-            conns.append(conn)
-            connected = True
+	global connected
+	while True:
+		if connected:
+			break
+		else:
+			conn, addr = sock.accept()
+			print(addr, 'has connected')
+			conns.append(conn)
+			connected = True
 
 
 if __name__ == '__main__':
-    threads = [threading.Thread(target=accept_connection),
-               threading.Thread(target=recv_data)]
-    for thread in threads:
-        thread.start()
-    print('started threads')
+	threads = [threading.Thread(target=accept_connection),
+			   threading.Thread(target=recv_data)]
+	for thread in threads:
+		thread.start()
+	print('started threads')
